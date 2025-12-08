1d5815efbf413d44acc805a003e120a2174b8797
yakhawaji-lang <y.a.khawaji@gmail.com>
2025-12-08 11:48:02 +0300

    إزالة import Header غير المستخدم

--- app/user/navigation/page.tsx
+++ app/user/navigation/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect, useRef } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
-import { Header } from '@/components/Header'
 import GoogleTrafficMap from '@/components/GoogleTrafficMap'
 import { 
   ArrowLeft, 
